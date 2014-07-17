9fccdefbdafe2d0bb3c0591333dc20394f988f51
Allan Simon <simona@gobeta.com.cn>
2014-07-17 22:58:44 +0800

    added support for pong when receiving ping from client

--- IqRouter.rs
+++ IqRouter.rs
@@ -14,10 +14,11 @@ pub fn route_iq (
     match ::IqParser::get_iq_first_child(iq).as_slice() {
         "bind" => treat_resource_binding(iq, stream),
         "session" => treat_session(iq, stream),
+        "ping" => treat_ping(iq, stream),
         _ => {
             println!("iq not treated!");
             println!("{}", iq);
-            send_dummy_result(iq, stream);
+            send_dummy_iq_result(iq, stream);
             return;
         }
     }
@@ -55,13 +56,23 @@ fn treat_session(
     sessionIq: &str,
     stream: &mut TcpStream
 ) {
-    send_dummy_result(sessionIq, stream);
+    send_dummy_iq_result(sessionIq, stream);
 }
 
+/// as of XEP 0199 replying a pong is simply
+/// replying a iq result
+///
+fn treat_ping(
+    sessionIq: &str,
+    stream: &mut TcpStream
+) {
+    send_dummy_iq_result(sessionIq, stream);
+}
 
+/// send on the wire a dummy iq of type result
+/// with the same ID
 ///
-///
-fn send_dummy_result (
+fn send_dummy_iq_result (
     iq: &str,
     stream : &mut TcpStream
 ) {
